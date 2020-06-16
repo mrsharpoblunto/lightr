f13d8436034a1bdb3823a6ca114d3fa059c7eac5
Glenn <mr.sharpoblunto@gmail.com>
2020-06-15 20:51:12 -0700

    Added retries for startup, logging, prettier formatting

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/**
+ * @format
+ */
 const EventEmitter = require('events').EventEmitter;
 const Gpio = require('onoff').Gpio;
 const http = require('http');
@@ -5,236 +8,249 @@ const fork = require('child_process').fork;
 const path = require('path');
 const storage = require('node-persist');
 const {
-  bridgeIp, 
-  userId, 
-  a1Pin, b1Pin, toggle1Pin,
-  a2Pin, b2Pin, toggle2Pin,
-  a3Pin, b3Pin, toggle3Pin
+  bridgeIp,
+  userId,
+  a1Pin,
+  b1Pin,
+  toggle1Pin,
+  a2Pin,
+  b2Pin,
+  toggle2Pin,
+  a3Pin,
+  b3Pin,
+  toggle3Pin,
 } = require('./config.json');
 
-const FIELDS = {'hue': 1024, 'sat': 8, 'bri': 8};
-const SELECT_PATTERN = [1,2,3,2,1,3];
+const FIELDS = {hue: 1024, sat: 8, bri: 8};
+const SELECT_PATTERN = [1, 2, 3, 2, 1, 3];
 
 class RotaryEncoder extends EventEmitter {
-	constructor({
-    a1,b1,toggle1,
-    a2,b2,toggle2,
-    a3,b3,toggle3,
-  }) {
-		super();
-		this.gpio1A = new Gpio(a1, 'in', 'both');
-		this.gpio1B = new Gpio(b1, 'in', 'both');
-		this.gpio1Toggle = new Gpio(toggle1, 'in', 'rising', { debounceTimeout: 10 });
-		this.gpio2A = new Gpio(a2, 'in', 'both');
-		this.gpio2B = new Gpio(b2, 'in', 'both');
-		this.gpio2Toggle = new Gpio(toggle2, 'in', 'rising', { debounceTimeout: 10 });
-		this.gpio3A = new Gpio(a3, 'in', 'both');
-		this.gpio3B = new Gpio(b3, 'in', 'both');
-		this.gpio3Toggle = new Gpio(toggle3, 'in', 'rising', { debounceTimeout: 10 });
+  constructor({a1, b1, toggle1, a2, b2, toggle2, a3, b3, toggle3}) {
+    super();
+    this.gpio1A = new Gpio(a1, 'in', 'both');
+    this.gpio1B = new Gpio(b1, 'in', 'both');
+    this.gpio1Toggle = new Gpio(toggle1, 'in', 'rising', {debounceTimeout: 10});
+    this.gpio2A = new Gpio(a2, 'in', 'both');
+    this.gpio2B = new Gpio(b2, 'in', 'both');
+    this.gpio2Toggle = new Gpio(toggle2, 'in', 'rising', {debounceTimeout: 10});
+    this.gpio3A = new Gpio(a3, 'in', 'both');
+    this.gpio3B = new Gpio(b3, 'in', 'both');
+    this.gpio3Toggle = new Gpio(toggle3, 'in', 'rising', {debounceTimeout: 10});
     this._watch(1, this.gpio1A, this.gpio1B, this.gpio1Toggle);
     this._watch(2, this.gpio2A, this.gpio2B, this.gpio2Toggle);
     this._watch(3, this.gpio3A, this.gpio3B, this.gpio3Toggle);
-	}
+  }
 
-  _watch(index, gpioA,gpioB,gpioToggle) {
-		gpioA.watch((err, value) => {
-			if (err) {
-				this.emit('error', err);
-				return;
-			}
-			const a = value;
+  _watch(index, gpioA, gpioB, gpioToggle) {
+    gpioA.watch((err, value) => {
+      if (err) {
+        this.emit('error', err);
+        return;
+      }
+      const a = value;
 
-			try {
-				const b = gpioB.readSync();
-				if (a === b) {
-					this.emit('rotation'+index, 1);
-				} else {
-					this.emit('rotation'+index, -1);
-				}
-			} catch (ex) {
-				this.emit('error', ex);
-			}
-		});
-		gpioToggle.watch((err, value) => {
-			if (err) {
-				this.emit('error', err);
-				return;
-			}
-			this.emit('toggle'+index);
-		});
-
+      try {
+        const b = gpioB.readSync();
+        if (a === b) {
+          this.emit(`rotation${index}`, 1);
+        } else {
+          this.emit(`rotation${index}`, -1);
+        }
+      } catch (ex) {
+        this.emit('error', ex);
+      }
+    });
+    gpioToggle.watch((err, value) => {
+      if (err) {
+        this.emit('error', err);
+        return;
+      }
+      this.emit('toggle' + index);
+    });
   }
 }
 
 class HueAPI {
-	constructor(bridge, user) {
-		this.bridge = bridge;
-		this.user = user;
-	}
+  constructor(bridge, user) {
+    this.bridge = bridge;
+    this.user = user;
+  }
   getGroups() {
     return this._httpGet('/groups');
   }
-	getGroup(groupId) {
-		return this._httpGet('/groups/' + groupId);
-	}
-	putGroup(groupId, data) {
-		return this._httpPut('/groups/' + groupId + '/action', data);
-	}
-	_httpGet(path) {
-		return new Promise((resolve, reject) => {
-			http.get({
-				host: this.bridge,
-				port: 80,
-				path: '/api/' + this.user + path
-			}, res => {
-				res.setEncoding('utf8');
-				let body = '';
-				res.on('data', data => {
-					body += data;
-				});
-				res.on('end', () => {
-					let parsed; 
-					try {
-						parsed = JSON.parse(body);
-					} catch (ex) {
-					}
-					resolve({ statusCode: res.statusCode, body: parsed || body});
-				});
-			}).on('error', (e) => {
-				reject(e);
-			});
-		});
-	}
-	_httpPut(path, data) {
-		return new Promise((resolve, reject) => {
-			const req = http.request({
-				host: this.bridge,
-				port: 80,
-				method: 'PUT',
-				path: '/api/' + this.user + path,
-				headers: { 'Content-Type': 'application/json' }
-			}, res => {
-				res.setEncoding('utf8');
-				let body = '';
-				res.on('data', data => {
-					body += data;
-				});
-				res.on('end', () => {
-					let parsed; 
-					try {
-						parsed = JSON.parse(body);
-					} catch (ex) {
-					}
-					resolve({ statusCode: res.statusCode, body: parsed || body});
-				});
-			});
-			req.on('error', (e) => {
-				reject(e);
-			});
-			req.write(JSON.stringify(data));
-			req.end();
-		});
-	}
+  getGroup(groupId) {
+    return this._httpGet(`/groups/${groupId}`);
+  }
+  putGroup(groupId, data) {
+    return this._httpPut(`/groups/${groupId}/action`, data);
+  }
+  _httpGet(path) {
+    return new Promise((resolve, reject) => {
+      http
+        .get(
+          {
+            host: this.bridge,
+            port: 80,
+            path: '/api/' + this.user + path,
+          },
+          res => {
+            res.setEncoding('utf8');
+            let body = '';
+            res.on('data', data => {
+              body += data;
+            });
+            res.on('end', () => {
+              let parsed;
+              try {
+                parsed = JSON.parse(body);
+              } catch (ex) {}
+              resolve({statusCode: res.statusCode, body: parsed || body});
+            });
+          },
+        )
+        .on('error', e => {
+          reject(e);
+        });
+    });
+  }
+  _httpPut(path, data) {
+    return new Promise((resolve, reject) => {
+      const req = http.request(
+        {
+          host: this.bridge,
+          port: 80,
+          method: 'PUT',
+          path: '/api/' + this.user + path,
+          headers: {'Content-Type': 'application/json'},
+        },
+        res => {
+          res.setEncoding('utf8');
+          let body = '';
+          res.on('data', data => {
+            body += data;
+          });
+          res.on('end', () => {
+            let parsed;
+            try {
+              parsed = JSON.parse(body);
+            } catch (ex) {}
+            resolve({statusCode: res.statusCode, body: parsed || body});
+          });
+        },
+      );
+      req.on('error', e => {
+        reject(e);
+      });
+      req.write(JSON.stringify(data));
+      req.end();
+    });
+  }
 }
 
-function throttlePromise(fn,{ reduce, debounce, delay } = { reduce: null, debounce: 0, delay: 0}) {
-	let running = false;
-	let waiting = false;
-	let start = Date.now();
-	let lastArgs = null;
-	let lastThis = null;
-	const completed = function() {
-		if (lastArgs != null) {
-			const nextArgs = lastArgs;
-			const nextThis = lastThis;
-			lastArgs = null;
-			lastThis = null;
-			throttled.apply(nextThis, nextArgs);
-		}
-	};
-	const throttled = function(...args) {
-		if (running || waiting) {
-			if (debounce && (Date.now() - start < debounce)) {
-				return;
-			}
-			lastThis = this;
-			if (reduce && lastArgs) {
-				lastArgs = reduce(lastArgs, args);
-			} else {
-				lastArgs = args;
-			}
-			return;
-		}
-		start = Date.now();
-		running = true;
-		if (delay) {
-			waiting = true;
-			const t = setTimeout(() => {
-				waiting = false;
-				if (!running) {
-					completed();
-				}
-			}, delay);
-		}
-		fn.apply(this, args).then((response) => {
-			running = false;
-			if (!waiting) {
-				completed();
-			}
-		}).catch((e) => {
-      console.log(e.stack);
-			running = false;
-			if (!waiting) {
-				completed();
-			}
-		});
-	};
-	return throttled;
+function throttlePromise(
+  fn,
+  {reduce, debounce, delay} = {reduce: null, debounce: 0, delay: 0},
+) {
+  let running = false;
+  let waiting = false;
+  let start = Date.now();
+  let lastArgs = null;
+  let lastThis = null;
+  const completed = function () {
+    if (lastArgs != null) {
+      const nextArgs = lastArgs;
+      const nextThis = lastThis;
+      lastArgs = null;
+      lastThis = null;
+      throttled.apply(nextThis, nextArgs);
+    }
+  };
+  const throttled = function (...args) {
+    if (running || waiting) {
+      if (debounce && Date.now() - start < debounce) {
+        return;
+      }
+      lastThis = this;
+      if (reduce && lastArgs) {
+        lastArgs = reduce(lastArgs, args);
+      } else {
+        lastArgs = args;
+      }
+      return;
+    }
+    start = Date.now();
+    running = true;
+    if (delay) {
+      waiting = true;
+      const t = setTimeout(() => {
+        waiting = false;
+        if (!running) {
+          completed();
+        }
+      }, delay);
+    }
+    fn.apply(this, args)
+      .then(response => {
+        running = false;
+        if (!waiting) {
+          completed();
+        }
+      })
+      .catch(e => {
+        console.log(e.stack);
+        running = false;
+        if (!waiting) {
+          completed();
+        }
+      });
+  };
+  return throttled;
 }
 
 class Worker {
-	constructor() {
-		this._createWorker();
-	}
-	_createWorker() {
-		this._worker = fork(path.resolve('worker.js'), [], {
-			stdio: ['pipe', 'pipe', 'pipe', 'ipc']
-		});
-		this._worker.stdout.on('data', (data) => {
-			console.log(data.toString('utf8'));
-		});
-		this._worker.stderr.on('data', (data) => {
-			console.log(data.toString('utf8'));
-		});
-		this._worker.on('exit', code => {
-			if (code === 1) {
-				console.log('Restarting Worker...');
-				this._createWorker();
-			}
-		});
-	}
-	send(mode, state) {
-		this._worker.send({ mode, state });
-	}
+  constructor() {
+    this._createWorker();
+  }
+  _createWorker() {
+    this._worker = fork(path.resolve('worker.js'), [], {
+      stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+    });
+    this._worker.stdout.on('data', data => {
+      console.log(data.toString('utf8'));
+    });
+    this._worker.stderr.on('data', data => {
+      console.log(data.toString('utf8'));
+    });
+    this._worker.on('exit', code => {
+      if (code === 1) {
+        console.log('Restarting Worker...');
+        this._createWorker();
+      }
+    });
+  }
+  send(mode, state) {
+    this._worker.send({mode, state});
+  }
 }
 
 class LightGroupController {
-	constructor(groupId, api, worker) {
-		this.groupId = groupId;
-		this.api = api;
-		this.worker = worker;
-		this.groupState = null;
+  constructor(groupId, api, worker) {
+    console.log('Controlling groupId: ' + groupId);
+    this.groupId = groupId;
+    this.api = api;
+    this.worker = worker;
+    this.groupState = null;
     this.matchIndex = 0;
-	}
+  }
 
-	async init() {
-		const {statusCode, body} = await this.api.getGroup(this.groupId);
+  async init() {
+    const {statusCode, body} = await this.api.getGroup(this.groupId);
     this.groupState = body.action;
     this.worker.send('lightgroup_control', body.action);
     return this.groupState;
-	}
+  }
 
-	async onEvent(event, args) {
+  async onEvent(event, args) {
     switch (event) {
       case 'rotation1':
         await this._rotate('bri', args);
@@ -254,7 +270,7 @@ class LightGroupController {
         }
 
         // check if the user has entered the select screen pattern
-        const index = parseInt(event[event.length - 1],10);
+        const index = parseInt(event[event.length - 1], 10);
         if (index === SELECT_PATTERN[this.matchIndex++]) {
           if (this.matchIndex === SELECT_PATTERN.length) {
             const newController = new LightGroupSelector(this.api, this.worker);
@@ -267,35 +283,38 @@ class LightGroupController {
         }
 
         const response = await this.api.putGroup(this.groupId, {
-          'on': this.groupState ? !this.groupState.on : true
+          on: this.groupState ? !this.groupState.on : true,
         });
         await this.init();
         break;
       }
     }
-	}
+  }
 
   async _rotate(field, value) {
     const change = {
       [field]: FIELDS[field] * value,
-      'on': true
+      on: true,
     };
     if (this.groupState) {
       Object.keys(change).forEach(k => {
         if (typeof change[k] === 'number') {
-          this.groupState[k] += change[k]
+          this.groupState[k] += change[k];
         } else {
-          this.groupState[k] = change[k]
+          this.groupState[k] = change[k];
         }
       });
-      this.worker.send('lightgroup_control',this.groupState);
+      this.worker.send('lightgroup_control', this.groupState);
     }
 
-    const response = await this.api.putGroup(this.groupId, Object.keys(change).reduce((prev, next) => {
-      const value = change[next];
-      prev[next + (typeof value  === 'number' ? '_inc' : '')] = value;
-      return prev;
-    }, {}));
+    const response = await this.api.putGroup(
+      this.groupId,
+      Object.keys(change).reduce((prev, next) => {
+        const value = change[next];
+        prev[next + (typeof value === 'number' ? '_inc' : '')] = value;
+        return prev;
+      }, {}),
+    );
     await this.init();
   }
 }
@@ -305,31 +324,33 @@ class LightGroupSelector {
     this.api = api;
     this.worker = uiWorker;
     this.selected = 0;
-
   }
   async init() {
     const response = await this.api.getGroups();
-    this.options = Object.keys(response.body).map(i => ({ key: parseInt(i, 10), value: response.body[i].name }));
-    this.worker.send('lightgroup_select', { 
-      selected: this.selected, 
-      options: this.options
+    this.options = Object.keys(response.body).map(i => ({
+      key: parseInt(i, 10),
+      value: response.body[i].name,
+    }));
+    this.worker.send('lightgroup_select', {
+      selected: this.selected,
+      options: this.options,
     });
   }
 
   async onEvent(event, args) {
     switch (event) {
       case 'rotation1':
-      case 'rotation2': 
+      case 'rotation2':
       case 'rotation3': {
-        this.selected += (args > 0 ? 1: -1);
+        this.selected += args > 0 ? 1 : -1;
         if (this.selected >= this.options.length) {
           this.selected = this.options.length - 1;
         } else if (this.selected < 0) {
           this.selected = 0;
         }
-        this.worker.send('lightgroup_select', { 
-          selected: this.selected, 
-          options: this.options
+        this.worker.send('lightgroup_select', {
+          selected: this.selected,
+          options: this.options,
         });
         break;
       }
@@ -340,13 +361,33 @@ class LightGroupSelector {
         const groupId = this.options[this.selected].key;
         console.log('Selected groupId: ' + groupId);
         await storage.setItem('groupId', groupId);
-        const newController = new LightGroupController(groupId, this.api, this.worker);
+        const newController = new LightGroupController(
+          groupId,
+          this.api,
+          this.worker,
+        );
         await newController.init();
         controller = newController;
         break;
       }
     }
   }
+}
+
+function retryUntilSuccess(action, onError) {
+  const retry = resolve => {
+    action()
+      .then(result => {
+        resolve(result);
+      })
+      .catch(err => {
+        onError(err);
+        setTimeout(() => retry(resolve), 1000);
+      });
+  };
+  return new Promise((resolve, reject) => {
+    retry(resolve);
+  });
 }
 
 let controller = null;
@@ -363,25 +404,48 @@ storage.init().then(async () => {
     toggle2: toggle2Pin,
     a3: a3Pin,
     b3: b3Pin,
-    toggle3: toggle3Pin
+    toggle3: toggle3Pin,
   });
 
   const groupId = await storage.getItem('groupId');
   if (!groupId) {
     controller = new LightGroupSelector(api, uiWorker);
   } else {
-    console.log('Controlling groupId: ' + groupId);
     controller = new LightGroupController(groupId, api, uiWorker);
   }
-  await controller.init();
 
-  for (let i = 1;i <= 3;++i) {
-    encoder.on('rotation'+i, throttlePromise((value) => controller.onEvent('rotation'+i, value), {
-      debounce: 100, 
-      delay: 500, 
-      reduce: (prev, next) => [prev[0] + next[0]]
-    }));
-    encoder.on('toggle'+i,() => controller.onEvent('toggle'+i));
+  // wait until the hue bridge responds
+  console.log('Attempting to contact Hue bridge...');
+  await retryUntilSuccess(
+    () => controller.init(),
+    err => console.error(err),
+  );
+
+  for (let i = 1; i <= 3; ++i) {
+    const rotationEvent = `rotation${i}`;
+    encoder.on(
+      rotationEvent,
+      throttlePromise(
+        value => {
+          if (process.env.NODE_ENV !== 'production') {
+            console.log(rotationEvent, value);
+          }
+          return controller.onEvent(rotationEvent, value);
+        },
+        {
+          debounce: 100,
+          delay: 500,
+          reduce: (prev, next) => [prev[0] + next[0]],
+        },
+      ),
+    );
+
+    const toggleEvent = `toggle${i}`;
+    encoder.on(toggleEvent, () => {
+      if (process.env.NODE_ENV !== 'production') {
+        console.log(toggleEvent);
+      }
+      return controller.onEvent(toggleEvent);
+    });
   }
-
 });
--- worker.js
+++ worker.js
@@ -1,3 +1,6 @@
+/**
+ * @format
+ */
 const i2c = require('i2c-bus');
 const i2cBus = i2c.openSync(1);
 const Oled = require('oled-i2c-bus');
@@ -8,8 +11,8 @@ class UIRenderer {
     this.oled = new Oled(i2cBus, {
       width: 128,
       height: 64,
-      address: 0x3C
-  }); 
+      address: 0x3c,
+    });
     this.renderers = {};
     this.currentMode = null;
     this.currentState = null;
@@ -19,78 +22,78 @@ class UIRenderer {
     return this;
   }
   render(newMode, newState) {
-      if (newMode !== this.currentMode) {
-        this.currentState = null;
-      }
+    if (newMode !== this.currentMode) {
+      this.currentState = null;
+    }
 
-			try {
-        const renderer = this.renderers[newMode];
-        renderer(this.currentState, newState, this.oled);
-        this.currentState = newState;
-			} catch (err) {
-        console.log(err.stack);
-				process.exit(1);
-			}
+    try {
+      const renderer = this.renderers[newMode];
+      renderer(this.currentState, newState, this.oled);
+      this.currentState = newState;
+    } catch (err) {
+      console.log(err.stack);
+      process.exit(1);
+    }
 
-      this.currentMode = newMode;
+    this.currentMode = newMode;
   }
 }
 
 function getDay(date) {
-	switch (date.getDay()) {
-		case 0:
-			return 'Sunday';
-		case 1:
-			return 'Monday';
-		case 2:
-			return 'Tuesday';
-		case 3: 
-			return 'Wednesday';
-		case 4:
-			return 'Thursday';
-		case 5:
-			return 'Friday';
-		case 6: 
-			return 'Saturday';
-	}
+  switch (date.getDay()) {
+    case 0:
+      return 'Sunday';
+    case 1:
+      return 'Monday';
+    case 2:
+      return 'Tuesday';
+    case 3:
+      return 'Wednesday';
+    case 4:
+      return 'Thursday';
+    case 5:
+      return 'Friday';
+    case 6:
+      return 'Saturday';
+  }
 }
 
 function getMonth(date) {
-	switch (date.getMonth()) {
-		case 0:
-			return 'January';
-		case 1:
-			return 'February';
-		case 2:
-			return 'March';
-		case 3: 
-			return 'April';
-		case 4:
-			return 'May';
-		case 5:
-			return 'June';
-		case 6: 
-			return 'July';
-		case 7: 
-			return 'August';
-		case 8: 
-			return 'September';
-		case 9: 
-			return 'October';
-		case 10: 
-			return 'November';
-		case 11: 
-			return 'December';
-	}
+  switch (date.getMonth()) {
+    case 0:
+      return 'January';
+    case 1:
+      return 'February';
+    case 2:
+      return 'March';
+    case 3:
+      return 'April';
+    case 4:
+      return 'May';
+    case 5:
+      return 'June';
+    case 6:
+      return 'July';
+    case 7:
+      return 'August';
+    case 8:
+      return 'September';
+    case 9:
+      return 'October';
+    case 10:
+      return 'November';
+    case 11:
+      return 'December';
+  }
 }
 
-process.on('uncaughtException', (err) => {
+process.on('uncaughtException', err => {
   console.error(err.stack);
   process.exit(1);
 });
 
 const uiRenderer = new UIRenderer()
-  .addRenderer('lightgroup_control', (prev,next, oled) => {
+  .addRenderer('lightgroup_control', (prev, next, oled) => {
     if (prev && JSON.stringify(next) === JSON.stringify(prev)) {
       return;
     }
@@ -108,35 +111,36 @@ const uiRenderer = new UIRenderer()
       oled.writeString(font, 2, 'L', 1, false);
     }
 
-    if (!prev || prev.on !== next.on) { 
+    if (!prev || prev.on !== next.on) {
       oled.setCursor(38, 1);
-      oled.writeString(font, 1, next.on ? 'On ': 'Off', 1, false);
+      oled.writeString(font, 1, next.on ? 'On ' : 'Off', 1, false);
     }
 
     const getProgress = (value, max) => {
-      return Math.max(0, Math.round((Math.min(value, max) / max) * (127-15)));
+      return Math.max(0, Math.round((Math.min(value, max) / max) * (127 - 15)));
     };
 
     if (!prev || prev.hue !== next.hue) {
       oled.fillRect(14, 18, 127, 14, 1);
       let hProgress = getProgress(next.hue, 65535.0);
-      oled.fillRect(15 + hProgress, 19, (127-15-hProgress), 12, 0);
+      oled.fillRect(15 + hProgress, 19, 127 - 15 - hProgress, 12, 0);
     }
 
     if (!prev || prev.sat !== next.sat) {
       oled.fillRect(14, 34, 127, 14, 1);
       let sProgress = getProgress(next.sat, 254.0);
-      oled.fillRect(15 + sProgress, 35, (127-15-sProgress), 12, 0);
+      oled.fillRect(15 + sProgress, 35, 127 - 15 - sProgress, 12, 0);
     }
 
     if (!prev || prev.bri !== next.bri) {
       oled.fillRect(14, 50, 127, 14, 1);
       let lProgress = getProgress(next.bri, 254.0);
-      oled.fillRect(15 + lProgress, 51, (127-15-lProgress), 12, 0);
+      oled.fillRect(15 + lProgress, 51, 127 - 15 - lProgress, 12, 0);
     }
 
     oled.update();
-  }).addRenderer('lightgroup_select', (prev, next, oled) => {
+  })
+  .addRenderer('lightgroup_select', (prev, next, oled) => {
     if (prev && JSON.stringify(prev) === JSON.stringify(next)) {
       return;
     }
@@ -154,27 +158,40 @@ const uiRenderer = new UIRenderer()
     let i = 0;
     for (let index = start; index < end; ++index) {
       if (index === selectedIndex) {
-        oled.setCursor(1, 18  + (8 * i));
+        oled.setCursor(1, 18 + 8 * i);
         oled.writeString(font, 1, '>', false);
       }
-      oled.setCursor(8, 18 + (8 * i++));
-      oled.writeString(font,1, next.options[index].value, 1, false);
+      oled.setCursor(8, 18 + 8 * i++);
+      oled.writeString(font, 1, next.options[index].value, 1, false);
     }
 
     oled.update();
-  }).addRenderer('clock', (prev, next, oled) => {
+  })
+  .addRenderer('clock', (prev, next, oled) => {
     if (prev && next.getMinutes() === prev.getMinutes()) {
       return;
     }
     oled.clearDisplay(false);
     oled.setCursor(1, 1);
-    oled.writeString(font, 1, getDay(next) + ' ' + getMonth(next) + ' ' + next.getDate(), 1, false);
+    oled.writeString(
+      font,
+      1,
+      `${getDay(next)} ${getMonth(next)} ${next.getDate()}`,
+      1,
+      false,
+    );
     oled.setCursor(11, 22);
-    oled.writeString(font, 4, next.getHours().toString().padStart(2,'0') + ':' + next.getMinutes().toString().padStart(2,'0'), 1, false);
+    oled.writeString(
+      font,
+      4,
+      `${next.getHours().toString().padStart(2, '0')}:${next.getMinutes().toString().padStart(2, '0')}`,
+      1,
+      false,
+    );
     oled.update();
 
     if (!prev) {
-      oled.startScroll('left', 0,1);
+      oled.startScroll('left', 0, 1);
       oled.dimDisplay(true);
     }
   });
@@ -183,25 +200,24 @@ const IDLE_TIME = 10000;
 let queuedMessage = null;
 let idleStart = Date.now();
 
-process.on('message', (message) => {
-	// rendering is real slow, so keep just the last UI state change around
-	// and ignore intermediary changes.
-	if (queuedMessage === null) {
-		// defer until the next tick so we give the process time to empty out
-		// any pending messages  before hogging the main thread again
-		process.nextTick(() => {
-			const m = queuedMessage;
-			queuedMessage = null;
+process.on('message', message => {
+  // rendering is real slow, so keep just the last UI state change around
+  // and ignore intermediary changes.
+  if (queuedMessage === null) {
+    // defer until the next tick so we give the process time to empty out
+    // any pending messages  before hogging the main thread again
+    process.nextTick(() => {
+      const m = queuedMessage;
+      queuedMessage = null;
       uiRenderer.render(m.mode, m.state);
-			idleStart = Date.now();
-		});
-	}
-	queuedMessage = message;
+      idleStart = Date.now();
+    });
+  }
+  queuedMessage = message;
 });
 
 setInterval(() => {
-	if (Date.now() - idleStart > IDLE_TIME) {
+  if (Date.now() - idleStart > IDLE_TIME) {
     uiRenderer.render('clock', new Date());
-	}
+  }
 }, 1000);
-
